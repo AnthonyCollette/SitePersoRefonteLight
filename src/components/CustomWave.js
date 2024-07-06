2302ea95ba835d303c2fed0a8f43d58c668ed407
Anthony Collette <100pour100inde@gmail.com>
2024-07-06 14:53:35 +0200

    style scrollbar

--- src/components/CustomWave.js
+++ src/components/CustomWave.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Wave from 'react-wavify';
 
-const CustomWave = ({color}) => {
+const CustomWave = ({ color }) => {
+
     return (
-        <Wave fill={color} options={{
-            amplitude: 50,
-            speed: 0.2,
-        }} />
+        <Wave fill={color} options={{ amplitude: 20, speed: 0.3 }} />
     );
 };
 
