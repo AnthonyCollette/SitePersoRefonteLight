62625bcf93a159d111d0440ca40c5ac85f5a9621
Anthony Collette <100pour100inde@gmail.com>
2025-01-08 14:38:44 +0100

    change router

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, { Suspense, lazy } from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import './assets/scss/main.scss';
-import { RouterProvider, createHashRouter } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { ThemeProvider } from './contexts/ThemeProvider';
 import Loading from './components/Loading';
 import NotFound from './components/NotFound';
@@ -15,7 +15,7 @@ const Blog = lazy(() => import('./views/Blog'))
 
 
 
-const router = createHashRouter([
+const router = createBrowserRouter([
   {
     path: '/',
     element: (
